b0f97ef518f85af44f4150a949421daf0987cd66
Adriana de Lazzari <adrianaclazzari@gmail.com>
2023-01-19 15:25:06 -0500

    Adding content to the recipes components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -4,12 +4,16 @@ import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe',
+      'This is a test',
+      'https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvYTAxOS1qYWt1YmstMDc5My1jcnVzdHktcGl6emEtd2l0aC1zYWxhbWktbXVzaHJvb21zLW9uaW9uLmpwZw.jpg?s=SdA7mVnOnC9UsgkaGFCuswqp8uoXe1NQz_1zgbSwAJQ'
+    ),
+  ];
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
